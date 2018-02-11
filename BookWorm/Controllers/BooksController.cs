a6cc96860e9ac18528732c60d9b3a389a3475161
Yamil Burgos <yamil.burgos1293@gmail.com>
2018-02-10 20:55:11 -0500

    Added Delete & Save Database Functionality For Books Table

--- BookWorm/Controllers/BooksController.cs
+++ BookWorm/Controllers/BooksController.cs
@@ -8,6 +8,12 @@ using BookWorm.Models;
 namespace BookWorm.Controllers {
 
     public class BooksController : Controller {
+		private BookEntity bookEntityContext;
+
+		public BooksController() {
+			bookEntityContext = new BookEntity();
+		}
+
 		public ActionResult Index() {
 			using (BookEntity BE = new BookEntity()) {
 				List<Books> bookList = new List<Books>();
@@ -34,47 +40,43 @@ namespace BookWorm.Controllers {
 		}
 
 		[HttpPost]
+		public ActionResult DeleteBook(int id) {
+			using (BookEntity BE = new BookEntity()) {
+				Books thisBook = BE.Books.SingleOrDefault(b => b.Id == id);
+				if (thisBook == null) return HttpNotFound();
+
+				bookEntityContext.Books.Attach(thisBook);
+				bookEntityContext.Books.Remove(thisBook);
+				bookEntityContext.SaveChanges();
+				return RedirectToAction("Index");
+			}
+		}
+
+		[HttpPost]
 		public ActionResult SaveBooks(Books books) {
-			//// Changes the flow of the program by using validation
-			//// data. Still return the same view if it isn't valid.
-			//if (!ModelState.IsValid)
-			//{
-			//	MovieFormViewModel viewModel = new MovieFormViewModel
-			//	{
-			//		// Takes the result given from the above query to use for the view.
-			//		Movie = movie,
-			//		// A query to contain all genres available via a list.
-			//		Genres = _context.Genres.ToList()
-			//	};
+			if (!ModelState.IsValid) {
+				Books bookList = new Books();
 
-			//	// Still return MovieForm's View page to visit. Also passes viewModel data.
-			//	return View("MovieForm", viewModel);
-			//}
+				return View("BookForm", bookList);
+			}
 
-			//if (movie.Id == 0)
-			//{
-			//	// Done for new movies who yet to have an id.
-			//	// Added to the dbContext memory, not the database!
-			//	movie.DateAdded = DateTime.Now;
-			//	_context.Movies.Add(movie);
-			//}
+			if (books.Id == 0) {
+				bookEntityContext.Books.Add(books);
+			}
 
-			//else
-			//{
-			//	// First query the database to find the movie with this existing id.
-			//	Movie movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
-			//	// Then pass along the movie's new information to this chosen variant.
-			//	movieInDb.Name = movie.Name;
-			//	movieInDb.GenreId = movie.GenreId;
-			//	movieInDb.NumberInStock = movie.NumberInStock;
-			//	movieInDb.ReleaseDate = movie.ReleaseDate;
-			//}
+			else {
+				Books bookInDb = bookEntityContext.Books.Single(b => b.Id == books.Id);
 
-			//// This will save the changes to the database.
-			//_context.SaveChanges();
-			//// Redirects the user back to the list of movies (to Index).
-			//return RedirectToAction("Index", "Movies");
-			return View();
+				bookInDb.Title = books.Title;
+				bookInDb.Author = books.Author;
+				bookInDb.CoverArt = books.CoverArt;
+				bookInDb.Description = books.Description;
+				bookInDb.Rating = books.Rating;
+				bookInDb.BookSeries = books.BookSeries;
+			}
+
+			bookEntityContext.SaveChanges();
+			return RedirectToAction("Index", "Books");
 		}
 	}
 }
